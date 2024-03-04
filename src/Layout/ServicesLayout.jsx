1a8689d093d66d5b651170275f1e60786ac5e5b6
Curtis Officer <officercd@yahoo.com>
2024-03-04 15:28:03 -0600

    fix ServicesLayout.jsx links

--- src/Layout/ServicesLayout.jsx
+++ src/Layout/ServicesLayout.jsx
@@ -3,6 +3,7 @@ import commercialIcon from "../assets/commercial_icon.png";
 import leafIcon from "../assets/leafremoval_icon.png";
 import hedgeIcon from "../assets/hedgetrimming_icon.png";
 import LazyLoad from "react-lazy-load";
+import { Link } from "react-router-dom";
 
 function ServiceCards() {
   return (
@@ -14,7 +15,11 @@ function ServiceCards() {
             Expert care, affordable pricing, and a commitment to excellence
           </div>
         </div>
-        <div className="mr-4 flex h-[200px] w-fit flex-col overflow-hidden rounded-br-[50px] rounded-tl-[50px] border-2 border-primary border-opacity-50 bg-background transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 hover:cursor-pointer hover:border-opacity-100 hover:bg-primary hover:bg-opacity-10">
+
+        <Link
+          to="/Tulsa-Lawn/services/residential-lawn"
+          className="mr-4 flex h-[200px] w-fit flex-col overflow-hidden rounded-br-[50px] rounded-tl-[50px] border-2 border-primary border-opacity-50 bg-background transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 hover:cursor-pointer hover:border-opacity-100 hover:bg-primary hover:bg-opacity-10"
+        >
           <div className="flex w-full justify-end">
             <div className="mr-5 pl-1 text-center font-text text-xl font-bold text-primary">
               RESIDENTIAL LAWN CARE
@@ -34,9 +39,12 @@ function ServiceCards() {
               a pristine outdoor canvas, season after season.
             </div>
           </div>
-        </div>
+        </Link>
 
-        <div className="ml-4 mt-5 flex h-[200px] w-fit flex-col overflow-hidden rounded-bl-[50px] rounded-tr-[50px] border-2 border-primary border-opacity-50 bg-background transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 hover:cursor-pointer hover:border-opacity-100 hover:bg-primary hover:bg-opacity-10">
+        <Link
+          to="/Tulsa-Lawn/services/commercial-lawn"
+          className="ml-4 mt-5 flex h-[200px] w-fit flex-col overflow-hidden rounded-bl-[50px] rounded-tr-[50px] border-2 border-primary border-opacity-50 bg-background transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 hover:cursor-pointer hover:border-opacity-100 hover:bg-primary hover:bg-opacity-10"
+        >
           <div className="flex w-full justify-start">
             <div className="ml-5 pl-1 text-center font-text text-xl font-bold text-primary">
               COMMERCIAL LAWN CARE
@@ -49,17 +57,18 @@ function ServiceCards() {
               experts provide meticulous maintenance, landscape perfection, and
               custom care for a standout business presence.
             </div>
-            <div
-              className="mr-2 h-[105px] w-1/4 scale-x-[-1] items-center overflow-hidden bg-contain bg-center bg-no-repeat"
-            >
+            <div className="mr-2 h-[105px] w-1/4 scale-x-[-1] items-center overflow-hidden bg-contain bg-center bg-no-repeat">
               <LazyLoad offset={100}>
                 <img src={commercialIcon} alt="" />
               </LazyLoad>
             </div>
           </div>
-        </div>
+        </Link>
 
-        <div className="mr-4 mt-5 flex h-[200px] w-fit flex-col overflow-hidden rounded-br-[50px] rounded-tl-[50px] border-2 border-primary border-opacity-50 bg-background transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 hover:cursor-pointer hover:border-opacity-100 hover:bg-primary hover:bg-opacity-10">
+        <Link
+          to="/Tulsa-Lawn/services/leaf-removal"
+          className="mr-4 mt-5 flex h-[200px] w-fit flex-col overflow-hidden rounded-br-[50px] rounded-tl-[50px] border-2 border-primary border-opacity-50 bg-background transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 hover:cursor-pointer hover:border-opacity-100 hover:bg-primary hover:bg-opacity-10"
+        >
           <div className="flex w-full justify-end">
             <div className="mr-5 pl-1 text-center font-text text-xl font-bold text-primary ">
               LEAF REMOVAL
@@ -79,32 +88,32 @@ function ServiceCards() {
               lawn, ensuring your property stays immaculate throughout the fall.
             </div>
           </div>
-        </div>
+        </Link>
 
-        <div className="ml-4 mt-5 flex h-[200px] w-fit flex-col overflow-hidden rounded-bl-[50px] rounded-tr-[50px] border-2 border-primary border-opacity-50 bg-background transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 hover:cursor-pointer hover:border-opacity-100 hover:bg-primary hover:bg-opacity-10">
+        <Link
+          to="/Tulsa-Lawn/services/hedge-trimming"
+          className="ml-4 mt-5 flex h-[200px] w-fit flex-col overflow-hidden rounded-bl-[50px] rounded-tr-[50px] border-2 border-primary border-opacity-50 bg-background transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 hover:cursor-pointer hover:border-opacity-100 hover:bg-primary hover:bg-opacity-10"
+        >
           <div className="flex w-full justify-start">
             <div className="ml-5 pl-1 text-center font-text text-xl font-bold text-primary">
-            HEDGE TRIMMING
+              HEDGE TRIMMING
             </div>
           </div>
 
           <div className="flex h-full w-full items-center">
             <div className="ml-1 flex h-full w-3/4 items-center px-1 text-start font-text text-sm">
-            Experience precision with our hedge trimming service. We sculpt
+              Experience precision with our hedge trimming service. We sculpt
               and shape your hedges to perfection, providing a clean, defined
               look that complements your property's aesthetic.
             </div>
-            <div
-              className="mr-2 h-[105px] w-1/4 scale-x-[-1] items-center overflow-hidden bg-contain bg-center bg-no-repeat"
-            >
+            <div className="mr-2 h-[105px] w-1/4 scale-x-[-1] items-center overflow-hidden bg-contain bg-center bg-no-repeat">
               <LazyLoad offset={100}>
                 <img src={hedgeIcon} alt="" />
               </LazyLoad>
             </div>
           </div>
-        </div>
+        </Link>
 
-      
         <div className="h-20"></div>
       </div>
     </>
