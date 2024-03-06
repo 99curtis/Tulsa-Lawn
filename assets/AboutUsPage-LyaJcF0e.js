e24da1cbffc9f0a9665222f8f7173633bf14c99e
Curtis Officer <officercd@yahoo.com>
2024-03-05 21:25:35 -0600

    Updates

---
+++ assets/AboutUsPage-LyaJcF0e.js
@@ -0,0 +1,1 @@
+import{j as e,M as s,N as r}from"./index-nrubGsfP.js";function a(){return e.jsx("div",{className:"bg-background min-h-screen w-screen overflow-x-hidden",children:e.jsxs(s,{children:[e.jsx(r,{}),e.jsx("div",{children:"About Us"})]})})}export{a as default};
