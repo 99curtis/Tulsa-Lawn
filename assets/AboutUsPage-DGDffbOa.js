30a228844e28316c88c5107ef40bd7a267cc337b
Curtis Officer <officercd@yahoo.com>
2024-03-08 10:23:50 -0600

    Updates

--- assets/AboutUsPage-DGDffbOa.js
+++
@@ -1,1 +0,0 @@
-import{j as e,M as s,N as r}from"./index-DL_VrsRK.js";function a(){return e.jsx("div",{className:"bg-background min-h-screen w-screen overflow-x-hidden",children:e.jsxs(s,{children:[e.jsx(r,{}),e.jsx("div",{children:"About Us"})]})})}export{a as default};
